be968c8a2887dd3ececc65be8f88affd4f995a5e
UtopiaMint <admin@lkp111138.me>
2020-04-23 03:21:23 +0800

    Render individual items

--- assets/js/items_v3.js
+++ assets/js/items_v3.js
@@ -12,6 +12,41 @@ $(function () {
         ['necklace_select', 'necklace'],
         ['weapon_select', 'weapon'],
     ];
+    // [suffix, id]
+    let idMap = {
+        damages: {
+            spellPercent: ['%', 'Spell Damage'],
+            spellRaw: ['', 'Neutral Spell Damage'],
+            meleePercent: ['%', 'Main Attack Damage'],
+            meleeRaw: ['', 'Main Attack Neutral Damage'],
+        },
+        regen: {
+            healthPercent: ['%', 'Health Regen'],
+            healthRaw: ['', 'Health Regen'],
+            mana: ['/4s', 'Mana Regen'],
+            soulPoint: ['%', 'Soul Pint Regen']
+        },
+        steal: {
+            mana: ['/4s', 'Mana Steal'],
+            health: ['/4s', 'Life Steal']
+        },
+        others: {
+            attackSpeedBonus: [' Tier', 'Attack Speed'],
+            exploding: ['%', 'Exploding'],
+            healthBonus: ['', 'Health'],
+            jumpHeight: ['', 'Jump Height'],
+            lootBonus: ['%', 'Loot Bonus'],
+            lootQuality: ['%', 'Loot Quality'],
+            poison: ['/3s', 'Poison'],
+            reflection: ['%', 'Reflection'],
+            sprint: ['', 'Sprint'],
+            sprintRegen: ['', 'Sprint Regen'],
+            stealing: ['%', 'Stealing'],
+            thorns: ['%', 'Thorns'],
+            walkSpeed: ['%', 'Walk Speed'],
+            xpBonus: ['%', 'XP Bonus']
+        }
+    };
     // min dmg, max dmg, conversion, +def, -def
     let powderStats = {
         E: [['earth','air'],[3,6,17,2,1],[6,9,21,4,2],[8,14,25,8,3],[11,16,31,14,5],[15,18,38,22,9],[18,22,46,30,13]],
@@ -20,12 +55,45 @@ $(function () {
         F: [['fire','water'],[2,5,14,3,1],[4,8,16,5,2],[6,10,19,9,3],[9,13,24,16,5],[12,16,30,25,9],[15,19,37,36,13]],
         A: [['air','fire'],[2,6,11,3,1],[4,9,14,6,2],[7,10,17,10,3],[9,13,22,16,5],[13,18,28,24,9],[16,18,35,34,13]]
     };
+    let skillBounsPct = [
+         0.0, 1.0, 2.0, 2.9, 3.9, 4.9, 5.8, 6.7, 7.7, 8.6,
+         9.5,10.4,11.3,12.2,13.1,13.9,14.8,15.7,16.5,17.3,
+        18.2,19.0,19.8,20.6,21.4,22.2,23.0,23.8,24.6,25.3,
+        26.1,26.8,27.6,28.3,29.0,29.8,30.5,31.2,31.9,32.6,
+        33.3,34.0,34.6,35.3,36.0,36.6,37.3,37.9,38.6,39.2,
+        39.9,40.5,41.1,41.7,42.3,42.9,43.5,44.1,44.7,45.3,
+        45.8,46.4,47.0,47.5,48.1,48.6,49.2,49.7,50.3,50.8,
+        51.3,51.8,52.3,52.8,53.4,53.9,54.3,54.8,55.3,55.8,
+        56.3,56.8,57.2,57.7,58.1,58.6,59.1,59.5,59.9,60.4,
+        60.8,61.3,61.7,62.1,62.5,62.9,63.3,63.8,64.2,64.6,
+        65.0,65.4,65.7,66.1,66.5,66.9,67.3,67.6,68.0,68.4,
+        68.7,69.1,69.4,69.8,70.1,70.5,70.8,71.2,71.5,71.8,
+        72.2,72.5,72.8,73.1,73.5,73.8,74.1,74.4,74.7,75.0,
+        75.3,75.6,75.9,76.2,76.5,76.8,77.1,77.3,77.6,77.9,
+        78.2,78.4,78.7,79.0,79.2,79.5,79.8,80.0,80.3,80.5,80.8];
+    let elemIcons = {
+        earth: '✤',
+        thunder: '✦',
+        water: '❉',
+        fire: '✹',
+        air: '❋'
+    };
+    let elemColours = {
+        earth: 'dark_green',
+        thunder: 'yellow',
+        water: 'aqua',
+        fire: 'red',
+        air: 'white'
+    };
+    let elementList = ['earth', 'thunder', 'water', 'fire', 'air'];
+    let skillList = ['strength', 'dexterity', 'intelligence', 'defense', 'agility'];
+    let itemListBox = $('#item_list_box');
+    let currentReq; // for the wearables
 
     console.log('window ready');
     // load item db
     loadItemDb().then(itemDb => {
         itemDb.forEach(item => globalItemDb[item.displayName || item.info.name] = item);
-        console.log(globalItemDb);
 
         // load into dropdown menus
         dropdowns.forEach((dropdown) => {
@@ -150,6 +218,218 @@ $(function () {
 
     function renderBuild(build) {
         console.log(build);
+    }
+
+    function generateItemBox(item, floatLeft) {
+        let itemBox;
+        let newLine = false;
+        if (floatLeft) {
+            itemBox = $('<div class="item float_left" style="position: absolute; top: 0; left: 0;"></div>');
+        } else {
+            itemBox = $('<div class="item"></div>');
+        }
+        // 1. name coloured according to tier
+        itemBox.append(`<span class="item_name ${item.info.tier.toLowerCase()}">${item.displayName || item.info.name}</span><br>`);
+        // 2. weapon attack speed
+        if (item.category === 'weapon') {
+            itemBox.append(`<span class="atk_speed">${capitalize(item.base.attackSpeed)} Attack Speed</span><br>`);
+        }
+        itemBox.append('<br>');
+        if (item.category !== 'weapon') {
+            // 3. armour health
+            if (item.base.health) {
+                itemBox.append(`<span class="armour_stats"><span class="mctext dark_red">❤ Health: ${nToString(item.base.health)}</span><br>`);
+                newLine = true;
+            }
+            // 4. armour defenses
+            elementList.forEach(elem => {
+                let value = item.base.defense[elem];
+                if (value) {
+                    itemBox.append(`<span class="mctext ${elemColours[elem]}">${elemIcons[elem]} ${capitalize(elem)}</span><span> Defense: ${nToString(value)}</span><br>`);
+                    newLine = true;
+                }
+            });
+        } else {
+            // 5. weapon damages
+            elementList.forEach(elem => {
+                let value = item.base.damage[elem];
+                if (value !== '0-0') {
+                    itemBox.append(`<span class="mctext ${elemColours[elem]}">${elemIcons[elem]} ${capitalize(elem)}</span><span> Damage: ${value}</span><br>`);
+                    newLine = true;
+                }
+            });
+        }
+        // TODO 6. powder special
+        if (newLine) {
+            itemBox.append("<br>");
+            newLine = false;
+        }
+        // 7. requirements
+        // 7.1 quest req
+        if (item.req.quest) {
+            itemBox.append(`<span class="mctext green">✓ </span><span>Quest Req: ${item.req.quest}</span><br>`);
+            newLine = true;
+        }
+        // 7.2 class req
+        if (item.req.class) {
+            itemBox.append(`<span class="mctext green">✓ </span><span>Class Req: ${item.req.class}</span><br>`);
+            newLine = true;
+        }
+        // 7.3 combat lvl req
+        if (item.req.level) {
+            itemBox.append(`<span class="mctext green">✓ </span><span>Combat Lv. Min: ${item.req.level}</span><br>`);
+            newLine = true;
+        }
+        // 7.4 skill req
+        skillList.forEach(skill => {
+            let value = item.req[skill];
+            if (value) {
+                itemBox.append(`<span class="mctext green">✓ </span><span>${capitalize(skill)} Min: ${value}</span><br>`);
+                newLine = true;
+            }
+        });
+        if (newLine) {
+            itemBox.append('<br>');
+            newLine = false;
+        }
+        // 8. bonus skills
+        skillList.forEach(skill => {
+            let value = item.base.skill[skill];
+            if (value) {
+                itemBox.append(`<span class="mctext ${value > 0 ? "green" : "red"}">${nToString(value)}</span><span> ${capitalize(skill)}</span><br>`);
+                newLine = true;
+            }
+        });
+        if (newLine) {
+            itemBox.append('<br>');
+            newLine = false;
+        }
+        // 9. ids
+        {
+            // damages
+            {
+                elementList.forEach(elem => {
+                    let value = item.identification.damage[elem];
+                    if (value) {
+                        itemBox.append(`<span class="mctext ${value > 0 ? "green" : "red"}">${nToString(value)}%</span><span> ${capitalize(elem)} Damage</span><br>`);
+                        newLine = true;
+                    }
+                });
+                for (let i in idMap.damages) {
+                    if (idMap.damages.hasOwnProperty(i)) {
+                        let value = item.identification.damage[i];
+                        let line = idMap.damages[i];
+                        if (value) {
+                            itemBox.append(`<span class="mctext ${value > 0 ? "green" : "red"}">${nToString(value)}${line[0]}</span><span> ${line[1]}</span><br>`);
+                        }
+                    }
+                }
+            }
+            // defenses
+            {
+                elementList.forEach(elem => {
+                    let value = item.identification.defense[elem];
+                    if (value) {
+                        itemBox.append(`<span class="mctext ${value > 0 ? "green" : "red"}">${nToString(value)}%</span><span> ${capitalize(elem)} Defense</span><br>`);
+                        newLine = true;
+                    }
+                });
+            }
+            // regen
+            {
+                for (let i in idMap.regen) {
+                    if (idMap.regen.hasOwnProperty(i)) {
+                        let value = item.identification.regen[i];
+                        let line = idMap.regen[i];
+                        if (value) {
+                            itemBox.append(`<span class="mctext ${value > 0 ? "green" : "red"}">${nToString(value)}${line[0]}</span><span> ${line[1]}</span><br>`);
+                        }
+                    }
+                }
+            }
+            // spell cost
+            {
+                let ordinals = ['1st', '2nd', '3rd', '4th'];
+                let spellCostPct = item.identification.spellCost.percent;
+                let spellCostRaw = item.identification.spellCost.raw;
+                for (let i = 0; i < 4; i++) {
+                    if (spellCostRaw[i]) {
+                        itemBox.append(`<span class="mctext ${spellCostRaw[i] < 0 ? "green" : "red"}">${nToString(spellCostRaw[i])}</span><span> ${ordinals[i]} Spell Cost</span><br>`);
+                        newLine = true;
+                    }
+                    if (spellCostPct[i]) {
+                        itemBox.append(`<span class="mctext ${spellCostPct[i] < 0 ? "green" : "red"}">${nToString(spellCostPct[i])}%</span><span> ${ordinals[i]} Spell Cost</span><br>`);
+                        newLine = true;
+                    }
+                }
+            }
+            // steal
+            {
+                for (let i in idMap.steal) {
+                    if (idMap.steal.hasOwnProperty(i)) {
+                        let value = item.identification.steal[i];
+                        let line = idMap.steal[i];
+                        if (value) {
+                            itemBox.append(`<span class="mctext ${value > 0 ? "green" : "red"}">${nToString(value)}${line[0]}</span><span> ${line[1]}</span><br>`);
+                        }
+                    }
+                }
+            }
+            // others
+            {
+                for (let i in idMap.others) {
+                    if (idMap.others.hasOwnProperty(i)) {
+                        let value = item.identification.others[i];
+                        let line = idMap.others[i];
+                        if (value) {
+                            itemBox.append(`<span class="mctext ${value > 0 ? "green" : "red"}">${nToString(value)}${line[0]}</span><span> ${line[1]}</span><br>`);
+                        }
+                    }
+                }
+            }
+        }
+        return itemBox;
+    }
+
+    function capitalize(s) {
+        return s.split('_').map(x => x.substr(0, 1).toUpperCase() + x.substr(1).toLowerCase()).join(' ');
+    }
+
+    function resetPos() {
+        const CONTAINER = '#item_list_box';
+        const SELECTOR = '.item.float_left';
+        const COLUMN_WIDTH = 250;
+        const VERTICAL_MARGIN = 40;
+        const HORIZONTAL_MARGIN = 24;
+
+        let container = $(CONTAINER);
+        let columns = Math.floor(container.width() / (HORIZONTAL_MARGIN + COLUMN_WIDTH));
+        let height_occupied = [];
+        let elems = container.children(SELECTOR);
+        container.css('position', 'relative');
+        for (let i = 0; i < columns; i++) {
+            height_occupied.push(0);
+        }
+        for (let i = 0; i < elems.length; i++) {
+            // find the col with least occupied height
+            let idx = 0;
+            let min = height_occupied[0];
+            for (let j = 1; j < columns; j++) {
+                if (height_occupied[j] < min) {
+                    min = height_occupied[j];
+                    idx = j;
+                }
+            }
+            let $elem = $(elems[i]);
+            $elem.css('position', 'absolute').css('top', min).css('left', COLUMN_WIDTH * idx + HORIZONTAL_MARGIN * idx);
+            height_occupied[idx] += $elem.height() + VERTICAL_MARGIN;
+        }
+        let h = Math.max.apply(this, height_occupied);
+        container.height(h);
+    }
+
+    function nToString(n) {
+        return (n > 0 ? '+' : '') + n;
     }
 
     function findStatReq(items) {
